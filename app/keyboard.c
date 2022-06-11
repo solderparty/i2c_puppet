d5b4fe9053d4930b2ba9765ac9b0ec9f9248bfd4
arturo182 <arturo182@tlen.pl>
2022-06-11 15:10:39 +0200

    Make sure buttons still work when Num Lock is on
    Fixes #10

--- app/keyboard.c
+++ app/keyboard.c
@@ -114,8 +114,9 @@ static void transition_to(struct list_item * const p_item, const enum key_state 
 				if (reg_is_bit_set(REG_ID_CFG, CFG_USE_MODS)) {
 					const bool shift = (self.mods[KEY_MOD_ID_SHL] || self.mods[KEY_MOD_ID_SHR]) | self.capslock;
 					const bool alt = self.mods[KEY_MOD_ID_ALT] | self.numlock;
+					const bool is_button = (key <= KEY_BTN_RIGHT1) || ((key >= KEY_BTN_LEFT2) && (key <= KEY_BTN_RIGHT2));
 
-					if (alt) {
+					if (alt && !is_button) {
 						key = p_entry->alt;
 					} else if (!shift && (key >= 'A' && key <= 'Z')) {
 						key = (key + ' ');
